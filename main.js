956a8f05a97d390bd9dbb99d23726b849d5f2ace
Veronika Bosáková <veronikabosakova@gmail.com>
2022-04-19 18:37:45 +0200

    Add localStorage communication

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // do proměnné si uložíme prvek, do kterého budeme vypisovat seznam [úkol]
 
 // založíme si prázdné pole, ve kterém budeme uchovávat úkoly
-
+let ukoly = [];
 // v poli budou úkoly jako objekty, každý úkol má popis a důležitost
 // když něco v poli bude, mělo by to vypadat takto:
 /*
@@ -20,20 +20,26 @@ ukoly = [
 
 // ihned po načtení stránky zavoláme funkci,
 // která přečte seznam úkolů z Local Storage
-
+nactiUkoly();
 // a hned seznam úkolů zobrazíme
 
 
 
 // funkce pro načtení seznamu úkolů z Local Storage
 function nactiUkoly() {
-
+  let hodnota = localStorage.ukoly;
+  console.log("call of nactiUkoly");
+  if(hodnota === null || hodnota === undefined) {
+    ukoly = [];
+  } else {
+    ukoly = JSON.parse(hodnota);
+  }
 }
 
 
 // funkce pro uložení seznamu úkolů do Local Storage
 function ulozUkoly() {
-
+  localStorage.ukoly = JSON.stringify(ukoly);;
 }
 
 
